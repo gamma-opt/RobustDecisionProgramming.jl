2fc06e4fed78254550cfe18605aa453a4f562f99
Jaan Tollander de Balsch <jaan@hey.com>
2020-12-07 18:02:49 +0200

    Working prototype of best worst case

---
+++ examples/exact_linear.jl
@@ -0,0 +1,56 @@
+using Random
+using JuMP
+using Gurobi
+using DecisionProgramming
+using RobustDecisionProgramming
+using Base.Iterators: product
+
+rng = MersenneTwister(22)
+
+S = States([2, 3])
+C = [ChanceNode(2, Node[1])]
+D = [DecisionNode(1, Node[])]
+V = [ValueNode(3, Node[1, 2])]
+X = [Probabilities(rng, c, S) for c in C]
+Y = [Consequences(rng, v, S) for v in V]
+
+validate_influence_diagram(S, C, D, V)
+sort!.((C, D, V, X, Y), by = x -> x.j)
+
+ks = [1]
+ϵs = [0.1]
+rs = [[x] for x in X]
+for (k, ϵ) in zip(ks, ϵs)
+    rs[k] = [Probabilities(X[k].j, d) for d in uncertainty_sets(X[k].data, ϵ)]
+end
+Xs = vec(map(x -> [x...], product(rs...)))
+
+U = DefaultPathUtility(V, Y)
+U⁺ = PositivePathUtility(S, U)
+
+model = Model()
+z = DecisionVariables(model, S, D)
+Ps = Vector{DefaultPathProbability}()
+EVs = Vector{GenericAffExpr}()
+for X′ in Xs
+    P = DefaultPathProbability(C, X′)
+    π_s = PathProbabilityVariables(model, z, S, P; hard_lower_bound=false)
+    EV = expected_value(model, π_s, U⁺)
+    push!(Ps, P)
+    push!(EVs, EV)
+end
+f = worst_case(model, EVs)
+@objective(model, Max, f)
+
+optimizer = optimizer_with_attributes(
+    () -> Gurobi.Optimizer(Gurobi.Env()),
+    "IntFeasTol" => 1e-9,
+)
+set_optimizer(model, optimizer)
+optimize!(model)
+
+@info("Extracting results.")
+Z = DecisionStrategy(z)
+
+@info("Printing decision strategy:")
+print_decision_strategy(S, Z)
--- src/RobustDecisionProgramming.jl
+++ src/RobustDecisionProgramming.jl
@@ -1,5 +1,72 @@
 module RobustDecisionProgramming
 
-greet() = print("Hello World!")
+using JuMP
+using Base.Iterators: product
+using Combinatorics: permutations
+
+export wasserstein_ball, uncertainty_set, uncertainty_sets, worst_case
+
+function wasserstein_ball(indices::Vector{Int}, p::Vector{Float64}, ϵ::Float64)
+    k = indices[end]
+    r = 2*ϵ
+    if r + p[k] > 1
+        r = 1 - p[k]
+    end
+    q = copy(p)
+    j = 1
+    σ = r
+    while σ > 0
+        i = indices[j]
+        δ = -min(σ, p[i])
+        σ += δ
+        q[i] += δ
+        j += 1
+    end
+    q[k] += r
+    return q
+end
+
+function uncertainty_set(p::Vector{Float64}, ϵ::Float64)::Vector{Vector{Float64}}
+    @assert all(p .≥ 0)
+    @assert isapprox(sum(p), 1)
+    @assert 0 ≤ ϵ ≤ 1
+    if iszero(ϵ)
+        return [p]
+    else
+        # Exclude iszero(p[i]) indices from the permutations
+        i = [i for i in LinearIndices(p) if !iszero(p[i])]
+        return [wasserstein_ball(indices, p, ϵ) for indices in permutations(i)]
+    end
+end
+
+function uncertainty_set_with_index(i, data, ϵ)
+    set = uncertainty_set(data[i, :], ϵ)
+    return collect(zip(fill(i, length(set)), set))
+end
+
+function combine(a::NTuple{K, Tuple{CartesianIndex{M}, Vector{Float64}}}, s::NTuple{N, Int})::Array{Float64, N} where {M, N, K}
+    @assert M == N - 1
+    p = zeros(s...)
+    for (i, v) in a
+        p[i, :] = v
+    end
+    return p
+end
+
+function uncertainty_sets(data::Array{Float64, N}, ϵ::Float64)::Vector{Array{Float64, N}} where N
+    # TODO: ϵs::Vector{Float64}, size: |S_j|, j::Node
+    s = size(data)
+    sets = [uncertainty_set_with_index(i, data, ϵ) for i in CartesianIndices(s[1:end-1])];
+    prods = vec(map(a -> combine(a, s), product(sets...)));
+    return prods
+end
+
+function worst_case(model, objs)
+    @variable(model, x)
+    for obj in objs
+        @constraint(model, x ≤ obj)
+    end
+    return x
+end
 
 end # module

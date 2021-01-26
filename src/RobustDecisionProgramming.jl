8423288463b75d0f4da10ceaca77c79a8953ea16
Jaan Tollander de Balsch <jaan@hey.com>
2021-01-26 12:04:24 +0200

    Cross assignment

--- src/RobustDecisionProgramming.jl
+++ src/RobustDecisionProgramming.jl
@@ -3,39 +3,42 @@ module RobustDecisionProgramming
 using JuMP
 using Combinatorics: permutations
 
-export cross_assignment, wasserstein_ball, uncertainty_set
+export cross_assignment, uncertainty_set
+
+# FIXME: assume increasing order for consistency with documentation
 
 function cross_assignment(l::Int, h::Int, d::Vector{Float64}, d⁻::Vector{Float64}, d⁺::Vector{Float64}, ϵ::Float64)
-    if (h - l ≤ 1) || (ϵ ≤ 0)
+    if (h - l ≤ 0) || (ϵ ≤ 0)
         return d
     end
     a = abs(d⁻[l] - d[l])
     b = abs(d⁺[h] - d[h])
     if a < b
         c = min(a, ϵ)
-        d[l] += c
-        d[h] -= c
+        d[l] -= c
+        d[h] += c
         return cross_assignment(l+1, h, d, d⁻, d⁺, ϵ - c)
     elseif a > b
         c = min(b, ϵ)
-        d[l] += c
-        d[h] -= c
+        d[l] -= c
+        d[h] += c
         return cross_assignment(l, h-1, d, d⁻, d⁺, ϵ - c)
     else
         c = min(a, ϵ)
-        d[l] += c
-        d[h] -= c
+        d[l] -= c
+        d[h] += c
         return cross_assignment(l+1, h-1, d, d⁻, d⁺, ϵ - c)
     end
 end
 
 function cross_assignment(p::Vector{Float64}, d⁻::Vector{Float64}, d⁺::Vector{Float64}, ϵ::Float64)
     k = length(p)
+    @assert k ≥ 1
     @assert length(p) == k
     @assert length(d⁻) == k
     @assert length(d⁺) == k
     @assert all(d⁻ .≥ -p)
-    @assert all(d⁺ .≤ 1-p)
+    @assert all(d⁺ .≤ 1.0.-p)
     @assert 0 ≤ ϵ ≤ 1
     d = zeros(k)
     cross_assignment(1, k, d, d⁻, d⁺, ϵ)
@@ -46,29 +49,10 @@ function cross_assignment(p::Vector{Float64}, d⁻::Vector{Float64}, d⁺::Vecto
 end
 
 function cross_assignment(p::Vector{Float64}, ϵ::Float64)
-    cross_assignment(p, -p, 1-p, ϵ)
+    cross_assignment(p, -p, 1.0.-p, ϵ)
 end
 
-function wasserstein_ball(indices::Vector{Int}, p::Vector{Float64}, ϵ::Float64)
-    k = indices[end]
-    r = 2*ϵ
-    if r + p[k] > 1
-        r = 1 - p[k]
-    end
-    q = copy(p)
-    j = 1
-    σ = r
-    while σ > 0
-        i = indices[j]
-        d = -min(σ, p[i])
-        σ += d
-        q[i] += d
-        j += 1
-    end
-    q[k] += r
-    return q
-end
-
+"""Polyhedral uncertainty set."""
 function uncertainty_set(p::Vector{Float64}, ϵ::Float64)::Vector{Vector{Float64}}
     @assert all(p .≥ 0)
     @assert isapprox(sum(p), 1)
@@ -76,9 +60,14 @@ function uncertainty_set(p::Vector{Float64}, ϵ::Float64)::Vector{Vector{Float64
     if iszero(ϵ)
         return [p]
     else
-        # Exclude iszero(p[i]) indices from the permutations
         i = [i for i in LinearIndices(p) if !iszero(p[i])]
-        return [wasserstein_ball(indices, p, ϵ) for indices in permutations(i)]
+        Q = Vector{Vector{Float64}}()
+        for indices in permutations(i)
+            q = copy(p)
+            q[indices] += cross_assignment(p[indices], ϵ)
+            push!(Q, q)
+        end
+        return Q
     end
 end
 

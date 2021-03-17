debdecdb0d3725a8cdfdd2b1eae28068a6d89735
Jaan Tollander de Balsch <jaan@hey.com>
2021-03-17 14:09:51 +0200

    Concrete test cases for polyhedral_uncertainty_set

--- test/runtests.jl
+++ test/runtests.jl
@@ -30,10 +30,18 @@ function test_polyhedral_uncertainty_set()
     p = [0.4, 0.6]
     d⁻ = [-0.2, -0.2]
     d⁺ = [0.2, 0.2]
-    polyhedral_uncertainty_set(Deviation(p, 0.1))
-    polyhedral_uncertainty_set(Deviation(p, d⁻, d⁺))
-    polyhedral_uncertainty_set(Deviation(p, d⁻, d⁺, 0.1))
-    @test true
+    @test isapprox(
+        polyhedral_uncertainty_set(Deviation(p, 0.1)),
+        [[0.3, 0.7], [0.5, 0.5]]
+    )
+    @test isapprox(
+        polyhedral_uncertainty_set(Deviation(p, d⁻, d⁺)),
+        [[0.2, 0.8], [0.6, 0.4]]
+    )
+    @test isapprox(
+        polyhedral_uncertainty_set(Deviation(p, d⁻, d⁺, 0.1)),
+        [[0.3, 0.7], [0.5, 0.5]]
+    )
 end
 
 test_cross_assignment()
